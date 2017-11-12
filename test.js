9629ca5f5869afd2328b8f5f2e6f3b1e7f1cf757
sciencefreak500 <gkm24@drexel.edu>
2017-11-11 19:01:11 -0500

    a json list to make it easy

--- test.js
+++ test.js
@@ -2,26 +2,45 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const cp = require("child_process");
+const jsonfile = require("jsonfile");
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', function(req,res){
 	res.send('listening');
-})
+});
 
 app.post('/webhook', function (req, res) {
   console.log(req.body.data);
-  switch(req.body.data){
+
+  var file = 'data.json';
+	jsonfile.readFile(file, function(err, obj) {
+	  for(var i in obj){
+	  	if(i == req.body.data){
+	  		console.log("running ", obj[i]);
+	  		cp.exec(obj[i]);
+	  		return;
+	  	}
+	  }
+	  console.log("not a command, add one?");
+	});
+
+
+
+  /*switch(req.body.data){
   	case "test":
   		console.log("test was activated");
   		cp.exec("C:/WINDOWS/system32/mspaint.exe");
   		break;
-
-  }
+  	case "eokoStart":
+  		console.log("eokoStart activated");
+  		cp.exec("C:/Users/Dreality/Desktop/eokoStart.bat");
+  }*/
  
 });
 
 app.listen(7050, () => {
 	console.log('Example app listening on port 7050!')
-})
+});
+
